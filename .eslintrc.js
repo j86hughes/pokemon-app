efd5aa0d555b51ec65a720e7cb57d7bf19303b1e
j86hughes <j86hughes@gmail.com>
2021-10-09 00:23:05 +0100

    added pre-commit hook for linting and added more logic fixes on App.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,19 @@ module.exports = {
   },
   plugins: ["react"],
   rules: {
-    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "react/jsx-filename-extension": [2, { extensions: [".js", ".jsx"] }],
+    strict: "off",
+    "object-curly-newline": [
+      "warn",
+      {
+        ObjectExpression: "always",
+        ObjectPattern: { multiline: true },
+        ImportDeclaration: "never",
+        ExportDeclaration: { multiline: true, minProperties: 3 },
+      },
+    ],
+    "jsx-quotes": ["warn", "prefer-double"],
+    "no-unused-vars": ["warn", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
+    "no-trailing-spaces": ["warn"],
   },
 };
--- src/App.js
+++ src/App.js
@@ -1,29 +1,49 @@
-import { React, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import Pokedex from 'pokedex-promise-v2';
 import './App.css';
-
-const Pokedex = require('pokedex-promise-v2');
 
 const P = new Pokedex();
 
-function App() {
+const getPokemonDetails = async (pokemon) => {
+  const item = await P.getPokemonByName(pokemon.name);
+  return item ? item : null;
+};
+
+const App = () => {
   const [pokemon, setPokemon] = useState([]);
   const [loading, setLoading] = useState(false);
+  const totalPokemon = 898;
 
-  const getPokemon = async ({ limit = 150, offset = 0 }) => {
+  const getRandomPokemon = async () => {
+    setLoading(true);
+
+    const pokemonList = await P.getPokemonsList({
+      limit: totalPokemon,
+      offset: 0,
+    });
+
+    const randomPokemonArray = [];
+    for (let i = 1; i < 13; i += 1) {
+      randomPokemonArray.push(pokemonList.results[Math.floor(Math.random() * totalPokemon)]);
+    }
+
+    const pokemonListWithDetails = await Promise.all(
+      randomPokemonArray.map(getPokemonDetails),
+    );
+
+    setPokemon(pokemonListWithDetails);
+    setLoading(false);
+  };
+
+  const getPokemon = async () => {
     setLoading(true);
     const pokemonList = await P.getPokemonsList({
-      limit,
-      offset,
+      limit: totalPokemon,
+      offset: 0,
     });
     if (pokemonList?.results) {
       const pokemonListWithDetails = await Promise.all(
-        pokemonList.results?.map(async (element) => {
-          const item = await P.getPokemonByName(element.name);
-          if (item) {
-            return item;
-          }
-          return null;
-        }),
+        pokemonList.results?.map(getPokemonDetails),
       );
 
       setPokemon(pokemonListWithDetails);
@@ -32,7 +52,7 @@ function App() {
   };
 
   useEffect(() => {
-    getPokemon({});
+    getPokemon();
   }, []);
 
   if (loading) {
@@ -42,29 +62,29 @@ function App() {
   return (
     <div className="App">
       <h1>Pokémon Pokedex Encyclopedia !</h1>
+      <button
+        type="button"
+        alt="Surprise me!"
+        onClick={getRandomPokemon}
+      >
+        Surprise me!
+      </button>
+      <button type="button" onClick={() => getPokemon()}>
+        Show all!
+      </button>
       <div className="container">
-        <button
-          type="button"
-          alt="Surprise me!"
-          onClick={() => getPokemon({
-            limit: 12,
-            offset: Math.floor(Math.random() * 150),
-          })}
-        >
-          Surprise me!
-        </button>
-        <button type="button" onClick={() => getPokemon({})}>
-          Show all!
-        </button>
         {pokemon?.map((pokemonItem) => (
           <div key={pokemonItem.name}>
-            <img alt={pokemonItem.name} src={pokemonItem.sprites.front_default} />
+            <img
+              alt={pokemonItem.name}
+              src={pokemonItem.sprites.front_default}
+            />
             <div>{pokemonItem.name}</div>
           </div>
         ))}
       </div>
     </div>
   );
-}
+};
 
 export default App;

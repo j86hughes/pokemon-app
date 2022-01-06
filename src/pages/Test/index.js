4aa98b65fe3b372212de849572f4c206497742df
j86hughes <j86hughes@gmail.com>
2022-01-06 20:40:34 +0000

    added react router dom boilerplate code

---
+++ src/pages/Test/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Test = () => (
+  <div>
+    You're doing it, Peter!!
+    YALALALALALALALALALALALALALALALALALALALALALALALALALALALALALALALALALALALALA
+  </div>
+);
+
+export default Test;

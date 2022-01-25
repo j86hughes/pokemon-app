5ba18db4d23e7fac7f2ac7be0d32766185cce1eb
Jim Boyade <anthonyradose@Noemies-MBP.lan>
2022-01-25 17:19:55 +0000

    fixed Type problem

--- src/pages/Pokemon/Type.js
+++ src/pages/Pokemon/Type.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './pokemon.css'
 
 
-const Type = (pokemonItem) => {
+const Type = ({pokemonItem}) => {
   let typesArray = pokemonItem.types;
     return(
         <div className='type-wrapper'>

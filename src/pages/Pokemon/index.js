ffdcdd08f27ff363ec000304d605d9b197b7b2ee
Jim Boyade <anthonyradose@Noemies-MBP.lan>
2022-02-25 19:01:06 +0000

    set loading state for pokemon page

--- src/pages/Pokemon/index.js
+++ src/pages/Pokemon/index.js
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Pokedex from "pokedex-promise-v2";
 
 import Heading from "./Heading";
 import Type from "./Type";
-import Nav from "./Nav";
 import Pagination from "./Pagination";
 import Stats from "./Stats";
 import Image from "./Image";
@@ -16,11 +15,18 @@ const Pokemon = () => {
   const { name } = useParams();
   const [pokemon, setPokemon] = useState();
   const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(false);
 
   const getPokemonDetails = async (name) => {
+    setLoading(true);
     const item = await P.getPokemonByName(name);
     setPokemon(item);
+    setLoading(false)
   };
+
+  if (loading) {
+    return <div className="pokeball" />;
+  }
 
   useEffect(() => {
     if (name) {
